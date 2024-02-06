1b61da5f5bae09c44794634e2315c55da8bfe2e7
eduardo <rm93199@fiap.com>
2024-02-05 21:31:56 -0300

    feat: add SecurityFilter

--- src/main/java/com/algaworks/algalog/config/security/SecurityConfiguration.java
+++ src/main/java/com/algaworks/algalog/config/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
-package com.algaworks.algalog.config;
+package com.algaworks.algalog.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,12 +12,15 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 
 public class SecurityConfiguration {
 
+    @Autowired
+    SecurityFilter securityFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
@@ -26,7 +30,9 @@ public class SecurityConfiguration {
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/produto").hasRole("ADMIN")
-                .anyRequest().authenticated())
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
